bcdd60d554319720dfa1bf62409858a45f0bb35c
Rasib Arshad <rasibarshad01@gmail.com>
2026-08-02 18:39:49 +0500

    Learned the concept of conditional expression (Ternary operator)

---
+++ conditional expression.py
@@ -0,0 +1,8 @@
+# conditional expression -> one-liner form of if-else statment as known as ternary operator
+
+num = 10
+
+print("Positive." if num > 0 else "Negative." )
+
+result = ("Even." if num % 2 == 0 else "Oddd.")
+print(f"The number is {result}")
