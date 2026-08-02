9bac7c21af953322e3afb68550de2a5eff9545d5
Rasib Arshad <rasibarshad01@gmail.com>
2026-08-02 18:50:27 +0500

    Performed some exercises to strengthen the concept of conditional expression

--- conditional expression.py
+++ conditional expression.py
@@ -4,5 +4,19 @@ num = 10
 
 print("Positive." if num > 0 else "Negative." )
 
-result = ("Even." if num % 2 == 0 else "Oddd.")
+result = ("Even." if num % 2 == 0 else "Odd.")
 print(f"The number is {result}")
+
+#exercise
+
+num1 = 3
+num2 = 4
+age = 16
+
+max_num = num1 if num1 > num2 else num2
+min_num = num1 if num1 < num2 else num2
+
+print(f"{max_num} is the maximum number.")
+print(f"{min_num} is the minimum number.")
+
+print("Adult." if age >= 18 else "Child.")
