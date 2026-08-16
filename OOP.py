47ae531448cc2ed51f8c32e79d3ec5bb938f26f0
Rasib Arshad <rasibarshad01@gmail.com>
2026-08-16 13:54:24 +0500

    Learned about del keyword, Inheritance, and its few types with practice exercises

--- OOP.py
+++ OOP.py
@@ -70,40 +70,90 @@
 # car1 = Car()
 # car1.start()
 
-# Encapsulation -> Wrapping data and methods in a single unit (object) -> capsule
+# # Encapsulation -> Wrapping data and methods in a single unit (object) -> capsule
 
-# Exercise
-class Account:
-    def __init__(self, balance:int, account_no):
-        self.balance = balance
-        self.account_no = account_no
+# # Exercise
+# class Account:
+#     def __init__(self, balance:int, account_no):
+#         self.balance = balance
+#         self.account_no = account_no
 
-    def debit(self, amount:int):
-        if self.balance >= amount:
-            self.balance -= amount
-        else:
-            print("Insufficient balance.")
+#     def debit(self, amount:int):
+#         if self.balance >= amount:
+#             self.balance -= amount
+#         else:
+#             print("Insufficient balance.")
 
-    def credit(self, amount:int):
-        self.balance += amount
+#     def credit(self, amount:int):
+#         self.balance += amount
 
-    def display_balance(self):
-        print("Your balance is: ", self.balance)
+#     def display_balance(self):
+#         print("Your balance is: ", self.balance)
 
-person1 = Account(5400, "RAS23")
-print("Before debit.")
-person1.display_balance()
-person1.debit(400)
-print("After debit.")
-person1.display_balance()
+# person1 = Account(5400, "RAS23")
+# print("Before debit.")
+# person1.display_balance()
+# person1.debit(400)
+# print("After debit.")
+# person1.display_balance()
 
-print("Before credit.")
-person1.display_balance()
-person1.credit(700)
-print("After credit.")
-person1.display_balance()
+# print("Before credit.")
+# person1.display_balance()
+# person1.credit(700)
+# print("After credit.")
+# person1.display_balance()
+
+# # del keyword -> Used to delete attribute of an object or an entire object
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+
+# s1 = Student("Rasib")
+# print(s1.name)
+# del s1 # object deleted
+# print(s1.name)
+
+# # Private attribute
+# class BankAccount:
+#     def __init__(self, account_no, __account_pass):
+#         self.account_no = account_no
+#         self.__account_pass = __account_pass # can only be accessed inside the class
+
+# user1 = BankAccount("1234", "abcde")
+# print(user1.account_no)
+# print(user1.__account_pass) # It becomes hidden for user
+
+# Inheritance -> One class (child) inherits the properties and methods of another class (parent)
+# Single Inheritance
+class Car: # Parent class
+    @staticmethod
+    def start():
+        print("Car started...")
+
+    @staticmethod
+    def stop():
+        print("Car stopped...")
+
+class ToyotaCar(Car): # Child class -> Inherits from Car (Parent class)
+    def __init__(self, brand):
+        self.brand = brand
+
+    @staticmethod
+    def demo():
+        print("Function of ToyotaCar class")
+
+# Multi-Level Inheritance
+class VitzCar(ToyotaCar): # Grand child -> Inherits from ToyotaCar (parent) -> Inherits form Car (grand parent)
+    def __init__(self, type):
+        self.type = type
+
+    @staticmethod
+    def demo1():
+        print("Function of vitzCar class")
 
 
-
-
-
+vitz1 = VitzCar("Electric (PHEV)")
+vitz1.start() # Function of Car (grandparent)
+vitz1.demo() # Function of ToyotaCar (parent)
+vitz1.demo1() # Function of VitzCar (child)
+vitz1.stop() # Function of Car (grandparent)
